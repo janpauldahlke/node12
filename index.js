1b641e59e5cb2e4ef2d47fba84bf19287902abae
janpauldahlke <janpauldahlke@gmail.com>
2019-04-23 21:26:03 +0200

    npm run make42

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ import make42 from "./es6/index.mjs";
 import o from "./common/index.cjs";
 
 console.log(make42());
-//console.log(o.make43.random());
+console.log(o.make43.random());
 console.log(o.make43.giveNumber());
